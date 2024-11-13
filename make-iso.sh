47a4f1329471be692fcc2037c575519161c642e8
Drew Jaegle <drew@jaegles.org>
2024-11-13 14:15:45 -0700

    Cleaned up YAML; added code for ISO cleanup

--- make-iso.sh
+++ make-iso.sh
@@ -2,10 +2,17 @@
 #
 # This script will create a new custom ISO
 #
-now=$(date '+%Y%m%d%H%M%S00')
+iso_name="ubuntu-24.04.1-auto-server.iso"
+if test -f $iso_name; then
+  echo "Cleaning up old ISO..."
+  rm $iso_name
+fi
+
+echo "Creating new ISO..."
+now=$(date "+%Y%m%d%H%M%S00")
 xorriso -as mkisofs -r \
--V 'Ubuntu-Server 24.04.1 LTS amd64' \
--o ubuntu-24.04.1-auto-server.iso \
+-V "Ubuntu-Server 24.04.1 LTS amd64" \
+-o $iso_name \
 --modification-date=$now \
 --grub2-mbr BOOT/1-Boot-NoEmul.img \
 --protective-msdos-label \
@@ -15,14 +22,14 @@ xorriso -as mkisofs -r \
 -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b BOOT/2-Boot-NoEmul.img \
 -appended_part_as_gpt \
 -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 \
--c '/boot.catalog' \
--b '/boot/grub/i386-pc/eltorito.img' \
+-c "/boot.catalog" \
+-b "/boot/grub/i386-pc/eltorito.img" \
 -no-emul-boot \
 -boot-load-size 4 \
 -boot-info-table \
 --grub2-boot-info \
 -eltorito-alt-boot \
--e '--interval:appended_partition_2_start_1351729s_size_10144d:all::' \
+-e "--interval:appended_partition_2_start_1351729s_size_10144d:all::" \
 -no-emul-boot \
 -boot-load-size 10144 \
 source-files
